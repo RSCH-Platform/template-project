9fbc959772e3d6d2cde1506cc58ea1d3cada0691
juniyasyos <juniyasyos@gmail.com>
2026-08-08 02:08:46 +0700

    fix: preserve component state and scroll on pagination

--- template-1/resources/js/Components/Dashboard/Pagination.jsx
+++ template-1/resources/js/Components/Dashboard/Pagination.jsx
@@ -11,15 +11,15 @@ export default function Pagination({ links }) {
                 {links.map((item, i) => {
                     return item.url != null ? (
                         item.label.includes('Previous') ? (
-                            <Link className={style} key={i} href={item.url}>
+                            <Link preserveState preserveScroll className={style} key={i} href={item.url}>
                                 <IconChevronLeft size={'20'} strokeWidth={'1.5'} />
                             </Link>
                         ) : item.label.includes('Next') ? (
-                            <Link className={style} key={i} href={item.url}>
+                            <Link preserveState preserveScroll className={style} key={i} href={item.url}>
                                 <IconChevronRight size={'20'} strokeWidth={'1.5'} />
                             </Link>
                         ) : (
-                            <Link className={`px-2 py-1 text-sm border  rounded-md text-gray-500 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-900 dark:border-gray-900 ${item.active ? 'bg-white text-gray-700 dark:bg-gray-900 dark:text-gray-50' : 'bg-white dark:bg-gray-950'}`} key={i} href={item.url}>
+                            <Link preserveState preserveScroll className={`px-2 py-1 text-sm border  rounded-md text-gray-500 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-900 dark:border-gray-900 ${item.active ? 'bg-white text-gray-700 dark:bg-gray-900 dark:text-gray-50' : 'bg-white dark:bg-gray-950'}`} key={i} href={item.url}>
                                 {item.label}
                             </Link>
                         )
